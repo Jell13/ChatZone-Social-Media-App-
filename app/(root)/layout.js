b6164a1a2c54d9fd046d7486bca522fa88b6e27b
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-03-16 13:41:39 -0700

    added the sign-in and sign-up using API Clerk

---
+++ app/(root)/layout.js
@@ -0,0 +1,36 @@
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import "../globals.css";
+import LeftSideBar from "@/components/layout/LeftSideBar";
+import RightSideBar from "@/components/layout/RightSideBar";
+import MainContainer from "@/components/layout/MainContainer";
+import TopBar from "@/components/layout/TopBar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "ChatZone",
+  description: "Next 14 Social Media App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-purple-2`}>
+          <main className="flex flex-row">
+            {children}
+            {/* <LeftSideBar/>
+            <MainContainer>
+              <TopBar/>
+              {children}
+            </MainContainer>
+            <RightSideBar/> */}
+          </main>
+          
+        </body>
+      </html>
+    </ClerkProvider>
+    
+  );
+}
